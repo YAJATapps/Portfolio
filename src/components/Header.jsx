204e3b38b998dc438ee2b9ced2d0765c75320a21
Yajat <yajatapps3@gmail.com>
2021-04-07 13:05:42 -0700

    Ensure that animation is applied to header image only

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default class Header extends Component {
     render() {
         return (
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+                <img src={logo} id="App-logo" alt="logo" />
                 <h1>Portfolio</h1>
             </header>
         )
