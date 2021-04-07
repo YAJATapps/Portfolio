d8bec20830d7c134be7615fd34a1d42176bff3e0
Yajat <yajatapps3@gmail.com>
2021-04-07 11:19:25 -0700

    Separate header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import logo from './../logo.svg';
+import './Header.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Portfolio</h1>
+            </header>
+        )
+    }
+}
