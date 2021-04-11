a3dd42c7b4f08bb26b3a012c94343183e36f8fa9
Yajat <yajatapps3@gmail.com>
2021-04-10 21:42:01 -0700

    Separate buttons for git, store and website

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,23 +18,23 @@ function App() {
       <Header />
 
       <ProjectsContainer>
-        <ProjectBox projectName='Postify' projectDescription='A basic social media website written with PHP. University Project for Advanced Web Development course.' projectBanner={postifySite} bannerAlt='Postify banner logo' projectUrl='https://github.com/YAJATapps/Postify' />
-        <ProjectBox projectName='Messenger' projectDescription='The frontend of a test messenger. Not completed.' projectBanner={messengerSite} bannerAlt='Messenger banner logo' projectUrl='https://github.com/YAJATapps/Messenger' />
+        <ProjectBox projectName='Postify' projectDescription='A basic social media website written with PHP. University Project for Advanced Web Development course.' projectBanner={postifySite} bannerAlt='Postify banner logo' buttons={{ github: 'https://github.com/YAJATapps/Postify' }} />
+        <ProjectBox projectName='Messenger' projectDescription='The frontend of a test messenger. Not completed.' projectBanner={messengerSite} bannerAlt='Messenger banner logo' buttons={{ github: 'https://github.com/YAJATapps/Messenger' }} />
       </ProjectsContainer>
 
       <ProjectsContainer>
-        <ProjectBox projectName='Pear Launcher Website' projectDescription='Website for Pear Launcher.' projectBanner={pearSite} bannerAlt='Pear Launcher Site banner logo' projectUrl='https://pearlauncher.github.io/about.html' />
-        <ProjectBox projectName='Weather Site' projectDescription='A react app that uses open weather map API.' projectBanner={weatherSite} bannerAlt='Weather site banner logo' projectUrl='https://github.com/YAJATapps/WeatherSite' />
+        <ProjectBox projectName='Pear Launcher Website' projectDescription='Website for Pear Launcher.' projectBanner={pearSite} bannerAlt='Pear Launcher Site banner logo' buttons={{ website: 'https://pearlauncher.github.io/about.html', github: 'https://github.com/PearLauncher/pearlauncher.github.io' }} />
+        <ProjectBox projectName='Weather Site' projectDescription='A react app that uses open weather map API.' projectBanner={weatherSite} bannerAlt='Weather site banner logo' buttons={{ github: 'https://github.com/YAJATapps/WeatherSite' }} />
       </ProjectsContainer>
 
       <ProjectsContainer>
-        <ProjectBox projectName='Pear Launcher' projectDescription='Android Launcher App.' projectBanner={pearBanner} bannerAlt='Pear Launcher banner logo' projectUrl='https://play.google.com/store/apps/details?id=com.pearlauncher.pearlauncher' />
-        <ProjectBox projectName='PicScan' projectDescription='Document Scanner App.' projectBanner={picscanBanner} bannerAlt='PicScan banner logo' projectUrl='https://play.google.com/store/apps/details?id=com.picscan' />
+        <ProjectBox projectName='Pear Launcher' projectDescription='Android Launcher App.' projectBanner={pearBanner} bannerAlt='Pear Launcher banner logo' buttons={{ playstore: 'https://play.google.com/store/apps/details?id=com.pearlauncher.pearlauncher' }} />
+        <ProjectBox projectName='PicScan' projectDescription='Document Scanner App.' projectBanner={picscanBanner} bannerAlt='PicScan banner logo' buttons={{ playstore: 'https://play.google.com/store/apps/details?id=com.picscan' }} />
       </ProjectsContainer>
 
       <ProjectsContainer>
-        <ProjectBox projectName='Weather App' projectDescription='University Project for Mobile App Development course.' projectBanner={weatherBanner} bannerAlt='Weather App banner logo' projectUrl='https://github.com/YAJATapps/WeatherApp' />
-        <ProjectBox projectName='Reminder App' projectDescription='University Project for Human computer interaction course.' projectBanner={reminderBanner} bannerAlt='Reminder App banner logo' projectUrl='https://github.com/YAJATapps/ReminderApp' />
+        <ProjectBox projectName='Weather App' projectDescription='University Project for Mobile App Development course.' projectBanner={weatherBanner} bannerAlt='Weather App banner logo' buttons={{ github: 'https://github.com/YAJATapps/WeatherApp' }} />
+        <ProjectBox projectName='Reminder App' projectDescription='University Project for Human computer interaction course.' projectBanner={reminderBanner} bannerAlt='Reminder App banner logo' buttons={{ github: 'https://github.com/YAJATapps/ReminderApp' }} />
       </ProjectsContainer>
 
       <Footer />
--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -7,18 +7,41 @@ export default class ProjectBox extends Component {
             <div className='Project-box'>
                 <img src={this.props.projectBanner} className="App-logo" alt={this.props.bannerAlt} />
 
+                <h1>{this.props.projectName}<br></br></h1>
+                <h3>{this.props.projectDescription}</h3>
+
                 <div className='Flex-column'>
-                    <div className='Project-info'>
-                        <h1>{this.props.projectName}<br></br></h1>
-                        <h3>{this.props.projectDescription}</h3>
-                    </div>
-                    <a
-                        className="App-link"
-                        href={this.props.projectUrl}
-                        target="_blank"
-                        rel="noopener noreferrer">
-                        Learn More
-                    </a>
+                    {
+                        this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('website') &&
+                        <a
+                            className="App-link"
+                            href={this.props.buttons.website}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Website
+                        </a>
+                    }
+                    {
+                        this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('playstore') &&
+                        <a
+                            className="App-link"
+                            href={this.props.buttons.playstore}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Play Store
+                        </a>
+                    }
+                    {
+                        this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('github') &&
+                        <a
+                            className="App-link"
+                            href={this.props.buttons.github}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Github
+                        </a>
+                    }
+
                 </div>
             </div>
         )
