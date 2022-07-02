f3e38bfa9fb6dd21a08d8414513c50a500e2bf7b
Yajat <yajatapps3@gmail.com>
2022-07-02 14:01:06 -0700

    Aws experience

--- src/components/SkillsBox.jsx
+++ src/components/SkillsBox.jsx
@@ -11,7 +11,8 @@ export default class SkillsBox extends Component {
                 className='About-box'>
                 <h1 className='About-text'>
                     <ul>
-                        <li>Languages: Java, JavaScript, Python (Proficient) Intermediate: Kotlin, C#, PHP, Swift, C++ AWS: Serverless platform, Virtual machines, API gateway, DNS, and Web app deployment</li>
+                        <li>Languages: Java, JavaScript, Python (Proficient) Intermediate: Kotlin, C#, PHP, Swift, C++</li>
+                        <li>AWS: Serverless platform, Virtual machines, API gateway, DNS, and Web app deployment</li>
                         <li>Web: React, NodeJS, FastAPI, jQuery, Bootstrap, Ajax, HTML5, CSS</li>
                         <li>Databases: MySQL, MongoDB, Oracle</li>
                         <li>Technologies: Bash, Git, Flutter, Firebase, Google cloud platform and Azure virtual machines</li>
