a146a045c61893699cc8e70c85bc9b6db3e9b579
Yajat <yajatapps3@gmail.com>
2022-07-02 12:06:28 -0700

    Add resume download button to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,43 @@
 import React, { Component } from 'react'
-import logo from './../img/logo.png';
 import './../css/Header.css';
+import resumeLink from './../assets/Yajat_Kumar-Resume.pdf';
 
 // The header with icon and text
 export default class Header extends Component {
+
+    downloadResume() {
+        const link = document.createElement('a');
+        link.href = resumeLink;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 
     // Render this component
     render() {
         return (
             <header
                 className="App-header">
-                <img
-                    src={logo}
-                    id="App-logo"
-                    alt="logo" />
-                <h1>
-                    Portfolio
-                </h1>
+                <div>
+                    <h1
+                        className="Div-center">
+                        YAJAT<br />KUMAR
+                    </h1>
+                    <h2
+                        className="Title-name">
+                        Software Engineer (Student)
+                    </h2>
+                    <div
+                        className="Div-center">
+                        <button
+                            className="Button"
+                            onClick={this.downloadResume}>
+                            Download Resume
+                        </button>
+                    </div>
+                </div>
             </header>
         )
     }
-    
+
 }
