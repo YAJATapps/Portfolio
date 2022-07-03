b361572e7e9d7b6a6386e74b12f17f2429fcc961
Yajat <yajatapps3@gmail.com>
2022-07-03 07:28:04 -0700

    Adjust margins for section boxes

--- src/components/SkillsBox.jsx
+++ src/components/SkillsBox.jsx
@@ -11,7 +11,7 @@ export default class SkillsBox extends Component {
                 className='About-box'>
                 <h1 className='About-text'>
                     <ul>
-                        <li>Languages: Java, JavaScript, Python (Proficient) Intermediate: Kotlin, C#, PHP, Swift, C++</li>
+                        <li>Languages: Java, JavaScript, Python (Proficient) <br/>Intermediate: Kotlin, C#, PHP, Swift, C++</li>
                         <li>AWS: Serverless platform, Virtual machines, API gateway, DNS, and Web app deployment</li>
                         <li>Web: React, NodeJS, FastAPI, jQuery, Bootstrap, Ajax, HTML5, CSS</li>
                         <li>Databases: MySQL, MongoDB, Oracle</li>
