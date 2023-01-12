b5eac3e27fcf572f8e275cf3c1191978b6567223
Yajat <yajatapps3@gmail.com>
2023-01-12 02:05:21 -0500

    Add a image carousel for website screenshots

---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import './../css/ImageCarousel.css';
+import './../css/carousel.css';
+
+// A container for image carousel
+export default class ImageCarousel extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // The states that contain items
+        this.state = {
+            carouselItems: [],
+            indicatorItems: [],
+        };
+    }
+
+    // Render this component
+    render() {
+        return (
+            <div id="myModal" className="modal">
+
+                { /* Modal content */}
+                <div className="modal-content">
+                    <span className="close">&times;</span>
+
+                    <div id="carouselImageIndicators" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            {this.state.indicatorItems}
+                        </div>
+                        <div className="carousel-inner">
+                            {this.state.carouselItems}
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselImageIndicators" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselImageIndicators" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        let tempCarousel = [];
+        let tempIndicators = [];
+
+        if (this.props.images !== undefined && this.props.images.length > 0) {
+            // Add the first image as active
+            tempCarousel.push(<div className="carousel-item active">
+                <img src={this.props.images[0]} className="d-block w-100" alt="..." />
+            </div>);
+
+            // Add remaining carousel image items
+            for (let i = 1; i < this.props.images.length; i++) {
+                tempCarousel.push(<div className="carousel-item">
+                    <img src={this.props.images[i]} className="d-block w-100" alt="..." />
+                </div>);
+            }
+
+            // Add the first indicator as active
+            tempIndicators.push(<button type="button" data-bs-target="#carouselImageIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>);
+
+            // Add remaining indicator items
+            for (let i = 1; i < this.props.images.length; i++) {
+                let indicator = <button type="button" data-bs-target="#carouselImageIndicators" data-bs-slide-to={i} aria-label={"Slide " + (i + 1)}></button>;
+                tempIndicators.push(indicator);
+            }
+        }
+
+        this.setState({
+            carouselItems: tempCarousel,
+            indicatorItems: tempIndicators
+        });
+
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = () => {
+            this.props.removeCarousel();
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = (event) => {
+            if (event.target == modal) {
+                this.props.removeCarousel();
+            }
+        }
+    }
+}
--- src/components/ProjectBox.jsx
+++ src/components/ProjectBox.jsx
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import './../css/ProjectBox.css';
+import ImageCarousel from './ImageCarousel';
 
 // A project box that contains project image, description and link buttons
 export default class ProjectBox extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // The state that contain modalItem
+        this.state = {
+            modalItem: 0
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+        this.removeCarousel = this.removeCarousel.bind(this);
+    }
 
     // Render this component
     render() {
@@ -24,6 +37,7 @@ export default class ProjectBox extends Component {
 
                 <div
                     className='Buttons-row'>
+
                     {
                         this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('website') &&
                         <a
@@ -33,6 +47,18 @@ export default class ProjectBox extends Component {
                             rel="noopener noreferrer">
                             Website
                         </a>
+                    }
+                    {
+                        this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('images') &&
+                        <a
+                            className="App-link"
+                            onClick={this.handleClick}>
+                            Website Screenshots
+                        </a>
+                    }
+                    {
+                        this.state.modalItem != 0 &&
+                        <ImageCarousel images={this.props.buttons.images} removeCarousel={this.removeCarousel} />
                     }
                     {
                         this.props.buttons !== undefined && this.props.buttons.hasOwnProperty('playstore') &&
@@ -58,6 +84,22 @@ export default class ProjectBox extends Component {
                 </div>
             </div>
         )
+    }
+
+    handleClick() {
+        this.setState({
+            modalItem: 1
+        });
+
+        setTimeout(() => {
+            document.getElementById("myModal").style.display = "block";
+        }, "500");
+    }
+
+    removeCarousel() {
+        this.setState({
+            modalItem: 0
+        });
     }
 
 }
