f9020e1343299076d8f42581803bfff1b58eb2bc
Yajat <yajatapps3@gmail.com>
2024-06-30 06:52:15 -0400

    stats

--- src/components/AboutBox.jsx
+++ src/components/AboutBox.jsx
@@ -12,7 +12,7 @@ export default class AboutBox extends Component {
                 <h1 className='About-text'>
                     I have graduated with a degree in Bachelor of Computing Science from Thompson Rivers University. <br />
                     I have experience with android mobile app development and web development. <br />
-                    My app <a href="https://play.google.com/store/apps/details?id=com.pearlauncher.pearlauncher">Pear Launcher</a> has over <b>half million downloads</b> and more than 10 thousand users. <br />
+                    My app <a href="https://play.google.com/store/apps/details?id=com.pearlauncher.pearlauncher">Pear Launcher</a> has over one million <b>downloads</b> and more than 30 thousand <b>users</b>. <br />
                     My open source projects are hosted on my <a href="https://github.com/YAJATapps"> GitHub</a>  repository. <br />
                     I have experience with developing apps for windows.
                 </h1>
